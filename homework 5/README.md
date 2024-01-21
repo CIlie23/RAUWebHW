f2322102a4a289cae7da1698f3f3bdb650e4906a
Ilie <152164398+CIlie23@users.noreply.github.com>
2024-01-21 16:41:10 +0200

    Delete homework 5 directory

--- homework 5/README.md
+++
@@ -1,6 +0,0 @@
-# Epic-College-Project
-The start of somethin...
-
-https://stackoverflow.com/questions/69910270/flask-how-to-implement-a-user-self-remove-function
-
-https://www.youtube.com/watch?v=dam0GPOAvVI
--- homework 5/main.py
+++
@@ -1,7 +0,0 @@
-from website import create_app #we can do this cause it's a python package
-
-app = create_app()
-
-if __name__ == '__main__': #if we run main we execute the file
-    app.run(debug=True) #running web server yay, turn off in production
-    
